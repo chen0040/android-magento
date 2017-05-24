2c9a00c690023b35bf7c7af34540f7fa883fefbb
xschen <xs0040@gmail.com>
2017-05-25 04:45:25 +0800

    Update icons

--- app/src/main/java/chen0040/github/com/androidmagentomobile/MainActivityFragment.java
+++ app/src/main/java/chen0040/github/com/androidmagentomobile/MainActivityFragment.java
@@ -13,6 +13,11 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -36,6 +41,19 @@ public class MainActivityFragment extends Fragment {
         webView.setWebViewClient(new WebViewClient(){
 
             private String currentUrl;
+            private Pattern pattern = Pattern.compile(".*/id/(\\d+)/cat_id/.*");
+            private int productId;
+
+            private List<String> modelIds = Arrays.asList("1d5285f2e0fd4211a27c8042496d5959",
+                    "311d052a9f034ba8bce55a1a8296b6f9",
+                    "66e4ced42d3d4a9f8598241de7491783",
+                    "aba23531911c45439067a6e0aaccad07",
+                    "8e75372b3ace4717b85fe6eb224c9550",
+                    "89c45d1d5dfa4876ba353c86007084b8",
+                    "f2129918d55646dbbaa8268367fa242e",
+                    "e215b6fd60ca48ddabaa77373f35176c",
+                    "66b4d99c026343ce8774e8a3b521246d"
+            );
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
@@ -59,8 +77,19 @@ public class MainActivityFragment extends Fragment {
                 Log.v(TAG, "user clicking link: " + url);
 
                 if(url.contains("sendfriend")){
-                    Snackbar.make(wView, "Open 3D model now!!!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                    //wView.loadUrl(currentUrl);
+                    Matcher matcher = pattern.matcher(url);
+                    while (matcher.find()) {
+                        productId = Integer.parseInt(matcher.group(1));
+                        System.out.println("product id: " + productId);
+                    }
+
+                    Snackbar.make(wView, "Open 3D model for Product with ProductId = " + productId, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                    String id = modelIds.get(productId % modelIds.size());
+
+
+                    String mockUrl = "https://sketchfab.com/models/".concat(id).concat("/embed");
+                    wView.loadUrl(mockUrl);
                 } else {
                     wView.loadUrl(url);
                 }
