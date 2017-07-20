cad815fe83c52f79b13c95784ac4a39a1aafedcd
xschen <xs0040@gmail.com>
2017-07-20 12:44:44 +0800

    unit test update

---
+++ app/src/test/java/chen0040/github/com/androidmagentomobile/AndroidMagentoLogContext.java
@@ -0,0 +1,9 @@
+package chen0040.github.com.androidmagentomobile;
+
+/**
+ * Created by chen0 on 4/7/2017.
+ */
+
+public class AndroidMagentoLogContext {
+    protected AndroidLogger logger = new AndroidLogger();
+}
---
+++ app/src/test/java/chen0040/github/com/androidmagentomobile/Mediator.java
@@ -0,0 +1,14 @@
+package chen0040.github.com.androidmagentomobile;
+
+
+/**
+ * Created by xschen on 12/6/2017.
+ */
+public class Mediator {
+
+   public static final String url = "http://j-clef-web-01.japaneast.cloudapp.azure.com/";
+   public static final String customerUsername = "xs0040@gmail.com";
+   public static final String customerPassword = "chen@0040";
+
+
+}
