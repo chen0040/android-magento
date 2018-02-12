404f81451ae9d3f6cada201c70c9228e2ee4086f
xschen <xs0040@gmail.com>
2018-02-12 11:55:15 +0800

    update credential for valid testing

--- app/src/main/java/chen0040/github/com/androidmagentomobile/modules/MagentoModule.java
+++ app/src/main/java/chen0040/github/com/androidmagentomobile/modules/MagentoModule.java
@@ -11,9 +11,11 @@ import com.github.chen0040.androidmagentoclient.AndroidMagentoClient;
 public class MagentoModule {
     private static final String TAG = "MagentoModule";
 
-    public static final String HOME_URL = "http://j-clef-web-01.japaneast.cloudapp.azure.com/";
-    public static final String USERNAME = "xs0040@gmail.com";
-    public static final String PASSWORD = "chen@0040";
+    public static final String HOME_URL =  "http://magento2.demo.ubertheme.com/";
+    public static final String USERNAME = "roni_cost@example.com";
+    public static final String PASSWORD = "roni_cost@example.com";
+
+
     private static MagentoModule instance;
     private AndroidMagentoClient client;
 
--- app/src/test/java/chen0040/github/com/androidmagentomobile/Mediator.java
+++ app/src/test/java/chen0040/github/com/androidmagentomobile/Mediator.java
@@ -6,9 +6,12 @@ package chen0040.github.com.androidmagentomobile;
  */
 public class Mediator {
 
-   public static final String url = "http://j-clef-web-01.japaneast.cloudapp.azure.com/";
-   public static final String customerUsername = "xs0040@gmail.com";
-   public static final String customerPassword = "chen@0040";
+   public static final String url = "http://magento2.demo.ubertheme.com/";
+   public static final String customerUsername = "roni_cost@example.com";
+   public static final String customerPassword = "roni_cost@example.com";
+
+   public static final String adminUsername = "megamenu";
+   public static final String adminPassword = "megamenu@123";
 
 
 }
